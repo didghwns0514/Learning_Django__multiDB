356968155b62f7867abbf52da69a97e5c95e2ded
HOJUNYANG <didghwns0514@gmail.com>
2021-02-16 22:27:03 +0900

    after -makemigration, saved

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'storeapp.apps.StoreappConfig',
+    'collectapp.apps.CollectappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
